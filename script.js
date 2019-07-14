1f55f628f341515ca6cb93757e9c0ac8339c6e23
areeshatehreem <50449369+areeshatehreem@users.noreply.github.com>
2019-07-14 13:52:52 +0500

    wip

---
+++ script.js
@@ -0,0 +1,970 @@
+let melaManagement = [
+    "sittingArramgment",
+    "rooms",
+    "familyHall",
+    "table",
+    "glass",
+    "plates",
+    "fokes",
+    "spoons",
+    "ketchepBottle",
+    "sauses",
+    "pans",
+    "drinks",
+    "tissue",
+    "straws",
+    "knife",
+    "dishSpoons",
+    "ovens"
+]
+
+let decoration = [
+    "statues",
+    "Glass Succulent",
+    "ligthningDecoration",
+]
+
+
+
+
+let timings = [{
+    deliveryTimingsInHours: 24,
+    resturantTimingsInHours: 24,
+    phoneNumber: 0332145775,
+    facebookEmail: "www.secondWife.com",
+    isDeliveryFree: true,
+    isOnlineDelivery: true,
+}
+
+]
+
+
+let melaWorkers = [{
+    name: "sajid",
+    position: "cheif",
+    phoneNumber: 0321546721,
+    email: "sajidKhan@gmail.com",
+    age: 35,
+    address: "nearAirport",
+    qualification: "intermediate",
+
+},
+
+
+{
+    name: "zafer",
+    position: "cheif",
+    phoneNumber: 032455461,
+    email: "zaferAlam@gmail.com",
+    age: 40,
+    address: "near saudabad",
+    qualification: "none",
+    skill: "cheifCourse",
+
+
+
+},
+
+
+{
+    name: "jaffer",
+    position: "cheif",
+    phoneNumber: 0210045541,
+    email: "jafferRiaz123@gmail.com",
+    age: 26,
+    address: "near quaidabad",
+    qualification: "matric",
+    skill: "cheifCourse",
+
+
+}]
+
+
+let melaWaiter = [{
+    name: "ali",
+    position: "waiter",
+    phoneNumber: 021006731,
+    email: "aliShazeb45@gmail.com",
+    age: 20,
+    address: "near quaidabad",
+    qualification: "none",
+
+
+},
+
+
+{
+    name: "junaid",
+    position: "waiter",
+    phoneNumber: 021001241,
+    email: "junaidKhan@gmail.com",
+    age: 31,
+    address: "near lasipara",
+    qualification: "matric",
+
+
+},
+
+
+{
+    name: "moiz",
+    position: "waiter",
+    phoneNumber: 0133345541,
+    email: "moizSaleem123@gmail.com",
+    age: 26,
+    address: "near abbasiMarket",
+    qualification: "Bcom",
+    isJob: "partTime",
+
+},
+
+
+{
+    name: "ashenAli",
+    position: "waiter",
+    phoneNumber: 0215636731,
+    email: "ashsen.aliKY@gmail.com",
+    age: 24,
+    address: "near modelColony",
+    qualification: "Mcom",
+},
+
+
+
+{
+    name: "jammie",
+    position: "waiter",
+    phoneNumber: 021457231,
+    email: "jammieNasir@gmail.com",
+    age: 18,
+    address: "near safoora",
+    qualification: "matric",
+},
+
+
+{
+    name: "shahmeer",
+    position: "waiter",
+    phoneNumber: 024556731,
+    email: "shahmeerburhan@gmail.com",
+    age: 28,
+    address: "near quaidabad",
+    qualification: "-",
+
+}
+]
+
+
+
+let melaOwner = [{
+    name: "muhmmadMirzaSahah",
+    martialStatus: " married",
+    age: 59,
+    qualification: "ACCA ",
+    phoneNumber: 033215467,
+    email: "MMS.Shahzeb@gmail.com",
+    address: "DHA",
+    religion: "islam",
+    experience: "16 years",
+    position: "owner",
+    isJob: true,
+    company: "asimJofa",
+
+
+
+},
+
+
+{
+
+    name: "fahadNaeem",
+    martialStatus: " married",
+    age: 63,
+    qualification: "BBA",
+    phoneNumber: 0232321467,
+    email: "fahadNAeem78@gmail.com",
+    address: "clifton",
+    religion: "islam",
+    experience: "11 years",
+    position: "CEO",
+
+
+},
+
+
+{
+    name: "shameerJahan",
+    martialStatus: " married",
+    age: 45,
+    qualification: "BBA",
+    phoneNumber: 0232321467,
+    email: "shameerJahan@gmail.com",
+    address: "gulshan",
+    religion: "islam",
+    position: "manager",
+    experience: "14 years",
+
+},
+
+
+
+{
+    name: "omerFahad",
+    martialStatus: " married",
+    age: 40,
+    qualification: "CA inComplete",
+    phoneNumber: 0232322367,
+    email: "omerFAhad@gmail.com",
+    address: "gulshan e johar",
+    religion: "islam",
+    position: "Accountant",
+    skill: " fastTyping",
+    experience: "four years",
+
+
+}]
+
+
+
+let items = [{
+    colddrink: [
+        "cocaCola",
+        "pepsi",
+        "string",
+        "sprite",
+        "fanta",
+        " 7up",
+        "juices",
+        "tins",
+        "mineralWater"
+    ]
+},
+
+
+{
+    burgers: [
+        "zinger",
+        "zingerCheeseBurger",
+        "simpleBurger",
+        "spiceyZinger",
+        "beefBurger",
+        "chickenCheeseBurger",
+
+
+    ]
+},
+
+{
+    pizzaFlavours: [
+        "Chicken Alfredo",
+        "bihariChicken",
+        "malaiBoti",
+        "tandooriChicken",
+        "creamyTikka",
+        "afganiChicken",
+        "veggieLover"
+    ]
+
+},
+
+
+{
+    rollParatha: [
+        "simpleRollParatha",
+        "maalaiParatha",
+        "zingerRollParatha",
+        "beefRoll",
+        "chickenRoll",
+        "muttonRoll",
+    ]
+},
+
+
+{
+    seaFood: [
+        "prawns",
+        "fish",
+        "fingerFish",
+        "fishWithKabab",
+        "Clam dishes.",
+        "Crab dishes.",
+        "Fish dishes.",
+        "Lobster dishes.",
+        "Octopus dishes.",
+        "Oyster dishes.",
+        "Shrimp dishes.",
+        "Squid dishes.",
+        "chickenChowmen",
+    ]
+
+},
+
+
+{
+    starter: [
+        "lasania",
+        "pasta",
+        "chickenLasania",
+        "vegeLAsania",
+        "chickenPasta",
+        "cheesePasta",
+        "vegePasta",
+        "cheeseLasania",
+        "drumStick",
+        "breadRolls",
+        "poataoSticks",
+        "Garlic Bread",
+        "mozzerallaSticks",
+
+    ]
+},
+
+
+{
+
+    parathas: [
+        "cheeseParatha",
+        "chickenParatha",
+        "malaiBotiParatha",
+        "vegeParatha",
+        "simpleParatha",
+        "roti",
+        "tandooriRoti",
+
+
+
+    ]
+
+
+
+},
+
+
+{
+
+    salan: [
+        "nihari",
+        "acharGosht",
+        "greenKarahi",
+        "whiteKarahi",
+        "keemma",
+        "tikkaBoti",
+        "seekhKabab",
+        "behariBoti",
+        "chapleeKabab",
+        "redKarahi",
+        "chickenMalaiBoti",
+        "butterChicken",
+        "koftaCurry",
+        "specialMasalaMuttonKarahi",
+        "hunterBeef",
+
+
+
+
+    ]
+},
+
+
+{
+    fries: [
+        "fries",
+        "masalaFries",
+        "whiteSauseFries",
+        "cheeseFries",
+
+    ]
+},
+
+
+{
+    rice: [
+        "briyani",
+        "pulao",
+        "Yakhni Wala Pulao",
+        "chineseRice",
+        "Egg Vegetable Fried Rice",
+
+    ]
+
+
+
+
+},
+
+
+{
+    desserts: [
+        "Milky Seviyan",
+        "Gajar Ka Halwa",
+        "LABE SHEEREEN",
+        "Rasmalai",
+        "Caramel Fruit Chaat",
+    ]
+},
+
+{
+    cakes: [
+        "bananaCake",
+        "cheeseCake",
+        "coconutCake",
+        "spongeCake",
+        "chocolateCake",
+        "pineappleCake",
+        "strawberryCake",
+        "cupCake",
+        "darkChocolateCake",
+        "kitkatCake",
+
+    ]
+},
+
+{
+    iceCream: [
+        "chocolate",
+        "strawberry",
+        "butterPecan",
+        "cheeseStrawberry",
+        "pistachio",
+        "vanilla",
+        "whiteFudge",
+        "choceAndNuts",
+        "golaGanda",
+        "faluda",
+    ]
+
+
+},
+
+{
+    shakes: [
+        "banana",
+        "cheeko",
+        "badaam",
+        "strawberry",
+        "pineApple",
+        "mango",
+        "vanilla",
+        "pista",
+
+    ]
+},
+
+{
+    BBQ: [
+        "beefSeekhKabab",
+        "beefGolaKabab",
+        "Beef Fry Kabab",
+        "chickenTikka",
+        "beefBehariBoti",
+        "chickenGreenTikka",
+        "chickenReshmiKabab",
+        "chickenTurkishKabab",
+        "fishTikka",
+    ]
+
+
+},
+
+
+
+{
+    coffee: [
+        "latte",
+        "Café au lait",
+        "Cappuccino",
+        "Espresso",
+        "Flat white",
+        "Long black",
+        "Caffè macchiato",
+
+    ]
+},
+
+
+{
+    chineseDishes: [
+        "Lamb kebabs",
+        " Biang Biang Noodles",
+        ". Xi’An Meat burger (Roujiamo)",
+        "Hot and sour dumpling soup (Suantang Shuijiao)",
+
+    ]
+},
+
+{
+    salads: [
+        "Panzenella",
+        "BBQ Potato Salad",
+        "Thai Soyabean In Cabbage Cups",
+        ".Cucumber, Black Olive and Mint Salad",
+        ".Cucumber, Black Olive and Mint Salad",
+    ]
+
+
+},
+
+
+{
+    golGappy: [
+        "meethayGolGappy",
+        "specialGolGappy",
+        "lahoriGolGappy",
+        "dahiBarry",
+        "khaatyMeethy",
+    ]
+}
+
+
+]
+
+
+
+
+let delivery = [{
+    name: "moshinAwan",
+    phoneNumber: 0332145321,
+    email: "moshinAwan@gmail.com",
+    address: "house  # 13 , Aisha Manzil",
+    age: 37,
+    isBike: false,
+    education: "graduaatedFromArts",
+    isOtherJob: false,
+    martialStatus: "married",
+    religion: "islam",
+    nationality: "pakistani",
+    position: "deliveryMan",
+    CNIC: 0321 - 456 - 878,
+    islisences: true,
+    jobTimingsInHours: [12, "am"]
+
+},
+
+
+{
+    name: "aliyanShabir",
+    phoneNumber: 0332145321,
+    email: "aliyan.2shabir@gmail.com",
+    address: "house  # 21 near jamshedTown",
+    age: 43,
+    isBike: false,
+    education: "economics",
+    isOtherJob: true,
+    martialStatus: "married",
+    religion: "islam",
+    nationality: "pakistani",
+    position: "deliveryMan",
+    CNIC: 0321 - 445 - 871,
+    islisences: true,
+    jobTimingsInHours: [12, "pm"]
+
+},
+
+
+{
+    name: "VihaanKapoor ",
+    phoneNumber: 0332145321,
+    email: "vihaanKapoor.1@gmail.com",
+    address: "house  #  , Murad Memon Goth",
+    age: 43,
+    isBike: false,
+    education: "economics",
+    isOtherJob: true,
+    martialStatus: "single",
+    religion: "nonMuslim",
+    nationality: "indian",
+    position: "deliveryMan",
+    islisences: true,
+    jobTimingsInHours: [12, "pm"],
+    isPartTimeJob: true,
+
+}
+]
+
+let branches = [
+    "azizabad",
+    "gulshan-e-iqbal",
+    "P.E.C.H.S",
+    "modelColony",
+    "sherShah",
+    "gulshan",
+    "clifton",
+    "D.H.A"
+]
+
+let eventPlaning = [
+    "eidUlAzha",
+    "eidUlFiter",
+    "celebrations"
+]
+
+
+
+let facilities = [
+    "airConditionerRooms",
+    "freeWifi",
+    "familyHall",
+]
+
+let entertainnments = [
+
+    "playArea",
+    "watchMatchOnLED",
+    "songs",
+    "joker",
+    "animatedCartoons",
+    "magicShow",
+    "puppetShow",
+    "jumpingCastle"
+]
+
+let games = [
+    "luckyDraw",
+    "findCoin",
+    "freeGifts",
+    "highJumpRace"
+]
+
+
+let gaurds = [{
+    name: "suffiyaanMir",
+    age: 45,
+    position: "asGard",
+    phoneNumber: 02134567153,
+    CNIC: 0441 - 565 - 123,
+    experienceAsGard: "twoYears",
+    martialStatus: "married",
+    religion: "islam",
+    nationality: "pakistani",
+    address: "house 12 , kalaboard",
+    isHaveGun: true,
+    dutyTimingInhours: [9, "pm"],
+    salary: 5000,
+
+},
+
+
+
+{
+
+    name: "shahbaazTariq",
+    age: 25,
+    position: "asGard",
+    phoneNumber: 0234577153,
+    CNIC: 0321 - 875 - 673,
+    experienceAsGard: "fourMonths",
+    martialStatus: "single",
+    religion: "islam",
+    nationality: "pakistani",
+    address: "house 20 , azizabad",
+    isHaveGun: true,
+    dutyTimingInhours: [9, "pm"],
+    salary: 5000,
+}
+]
+
+
+let swipper = [{
+
+    name: "shahbaazTariq",
+
+    age: 25,
+    position: "asGard",
+    phoneNumber: 0234577153,
+    CNIC: 0321 - 875 - 673,
+    experienceAsGard: "fourMonths",
+    martialStatus: "single",
+    religion: "islam",
+    nationality: "pakistani",
+    address: "house 20 , azizabad",
+    isHaveGun: true,
+    dutyTimingInhours: [9, "pm"],
+    salary: 5000,
+},
+
+
+{
+
+
+    name: "shahbaazTariq",
+    age: 25,
+    position: "asGard",
+    phoneNumber: 0234577153,
+    CNIC: 0321 - 875 - 673,
+    experienceAsGard: "fourMonths",
+    martialStatus: "single",
+    religion: "islam",
+    nationality: "pakistani",
+    address: "house 20 , azizabad",
+    isHaveGun: true,
+    dutyTimingInhours: [9, "pm"],
+    salary: 5000,
+
+}
+
+]
+
+
+let washRooms = 3;
+
+
+
+let stalls = [{
+    foodStall: [
+        "pizza",
+        "burger",
+        "golaGanda",
+        "iceCream",
+        "fries",
+        "rollParathas",
+        "golGappy",
+        "chaat",
+        "freshJuice",
+        "other",
+
+    ]
+
+
+},
+
+
+{
+
+    buffe: [
+        "salan",
+        "rice",
+        "BBQ",
+        "chinese",
+        "seaFood",
+
+    ]
+},
+
+
+{
+    gameStalls: [
+        "luckyDraw",
+        "throwTheBall",
+        "dartThrow",
+        "coldPotato",
+        "littleSquirtsShootingGallery",
+        "panko",
+        "duckPond"
+
+
+    ]
+},
+
+{
+    paintGallery: [
+        "drawing",
+        "sketchingPad",
+        "paintings",
+        "facePainting",
+
+    ]
+},
+
+
+{
+    booksstalls: [
+
+        "kidsStories",
+        "horrorStories",
+        "encyclopedias",
+        "generalKnowledge",
+        "others",
+
+    ]
+
+
+
+},
+
+{
+
+}
+]
+
+
+let womenSecurityGaurds = [
+    {
+        name: "saforaBegum",
+        age: 52,
+        position: "asSecurityGard",
+        phoneNumber: 0234512353,
+        CNIC: 0321 - 789 - 123,
+        experienceAsGard: "fiveYears",
+        martialStatus: "married",
+        religion: "islam",
+        nationality: "pakistani",
+        address: "house 20 , azizabad",
+        isHaveGun: true,
+        dutyTimingInhours: [9, "pm"],
+        salary: 4500,
+
+    },
+
+
+    {
+
+
+        name: "majida",
+        age: 40,
+        position: "asSecurityGard",
+        phoneNumber: 0234545613,
+        CNIC: 04321 - 135 - 124,
+        experienceAsGard: "fiveMonths",
+        martialStatus: "married",
+        gender: "female",
+        religion: "islam",
+        nationality: "pakistani",
+        address: "house 11 , karimabad",
+        isHaveGun: false,
+        dutyTimingInhours: [9, "pm"],
+        salary: 4500,
+    },
+]
+
+let rides = [
+    {
+        ride: "rolarCoster",
+        seats: 20,
+        ticketRs: 100,
+    },
+
+    {
+        ride: "discovery",
+        seats: 30,
+        ticketRs: 200,
+
+    },
+
+
+    {
+        ride: "cyclone",
+        seats: 10,
+        ticketRs: 50,
+        
+    },
+
+    {
+
+        ride: "liftChair",
+        seats: 16,
+        ticketRs: 200,
+    },
+
+
+    {
+        ride: "bigPendulumRide",
+        seats: 20,
+        ticketRs: 200,
+    },
+
+    {
+        ride: "piratesShip",
+        seats: 16,
+        ticketRs: 200,
+    },
+
+    {
+        ride: "flyChair",
+        seats: 16,
+        ticketRs: 200,
+
+    },
+
+    {
+        ride: "wormCoasterRide",
+        seats: 16,
+        ticketRs: 200,
+    },
+
+    {
+        ride: "carouselRide",
+        seats: 16,
+        ticketRs: 200,
+    },
+
+    {
+        ride: "dodgingCar",
+        seats: 16,
+        ticketRs: 200,
+    },
+
+    {
+        ride: "horrorHouse",
+        seats: 16,
+        ticketRs: 200,
+    },
+
+    {
+        ride: "crazyBus",
+        seats: 16,
+        ticketRs: 200,
+    },
+
+
+    {
+
+        ride: "doubleFly",
+        seats: 16,
+        ticketRs: 200,
+    }
+]
+
+
+
+let carParking = [{
+    area: [120, "sq:feet"],
+
+},
+
+{
+    parkingForBikes: [{
+        area: [120, "sq:feet"],
+    }
+
+    ]
+}
+]
+
+
+let totalExpense = [
+    "lightning",
+    "Glass Succulent",
+    "kitchenExpense",
+    "statueExpense",
+    "salariesExpense",
+    "utilitiesExpense",
+    "assetsExpense",
+    "suppliesExpense",
+    "otherExpense",
+    "rideExpense",
+    "repairExpense",
+
+
+]
+
+console.log(melaManagement);
+console.log(decoration);
+console.log(timings);
+console.log(melaWorkers);
+console.log(melaWaiter);
+console.log(melaOwner);
+console.log(items);
+console.log(delivery);
+console.log(branches);
+console.log(eventPlaning);
+console.log(facilities);
+console.log(entertainnments);
+console.log(games);
+console.log(gaurds);
+console.log(swipper);
+console.log(washRooms);
+console.log(stalls);
+console.log(womenSecurityGaurds);
+console.log(carParking);
+console.log(totalExpense);
+console.log(rides);
